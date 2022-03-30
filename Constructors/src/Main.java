ff623a5947c17a9fdf4eeac5ac17b96a3bad3607
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-03-29 19:18:55 -0700

    Add files via upload

---
+++ Constructors/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		Human human1 = new Human("James", 23, 170.0);
+		Human human2 = new Human("Christine", 23, 110.0);
+		
+		human1.eat();
+		human2.drink();
+		
+	}
+
+}
